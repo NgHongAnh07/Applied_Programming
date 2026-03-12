596ed50b9dba9ae049562bfaa13d5dff47a84616
Nokutenda Gift Remwa <gnremwa@gmail.com>
2026-03-12 15:50:21 +0200

    Update module.py

--- module.py
+++ module.py
@@ -1,1 +1,14 @@
 username = input("Enter your name")
+#user opens the app
+#user enters name and dob
+#user decides if they want to store their data
+#user tell the program what they want, options are compatibility, star sign 
+# user receives their data
+
+# class for data storage of the user
+# class for compatibility
+# class for star sign 
+# dataset of all star signs and their behaviors
+# dataset of user infomation 
+# dataset of intermittent messages to the user will receive MAYBE
+
